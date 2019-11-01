1031a3d20240ff92589eff3630599f3809798536
jporras1 <javi.jr2150@gmail.com>
2019-11-01 11:38:52 -0500

    Initial Commit

---
+++ alertPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alertPractice
+//
+//  Created by Javier Porras jr on 11/1/19.
+//  Copyright © 2019 Javier Porras jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
